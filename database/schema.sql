45fd09481a46732ea79a56bff66f61cfb710a651
arnavbabel <arnav.babel@gmail.com>
2026-04-10 16:19:02 -0700

    Updating schema.sql to create student, athleticadministrator, and users tables

--- database/schema.sql
+++ database/schema.sql
@@ -1,3 +1,17 @@
+CREATE TABLE IF NOT EXISTS student (
+  asurite   TEXT PRIMARY KEY,
+  email     TEXT UNIQUE NOT NULL,
+  password  TEXT NOT NULL,
+  eligible  BOOLEAN DEFAULT TRUE
+);
+
+CREATE TABLE IF NOT EXISTS athleticadministrator (
+  staffId       SERIAL PRIMARY KEY,
+  staffEmail    TEXT UNIQUE NOT NULL,
+  staffPassword TEXT NOT NULL,
+  staffRole     TEXT NOT NULL
+);
+
 CREATE TABLE IF NOT EXISTS users (
   id            SERIAL PRIMARY KEY,
   email         TEXT UNIQUE NOT NULL,
